fc0e542b20acc179c55417968c236c4a9f2d1f62
François DESCAVES <fdescaves@users.noreply.github.com>
2022-11-23 23:23:42 +0100

    Replace User32.SendMessage with User32.PostMessage

--- src/DofusBuddy/Core/Managers/WindowManager.cs
+++ src/DofusBuddy/Core/Managers/WindowManager.cs
@@ -33,8 +33,8 @@ namespace DofusBuddy.Core.Managers
         public void SendLeftClickToWindow(IntPtr windowHandle, int x, int y)
         {
             var lParam = (IntPtr)(x | (y << 16));
-            User32.SendMessage(windowHandle, User32.WindowMessage.WM_LBUTTONDOWN, new IntPtr(0x0001), lParam);
-            User32.SendMessage(windowHandle, User32.WindowMessage.WM_LBUTTONUP, new IntPtr(0x0000), lParam);
+            User32.PostMessage(windowHandle, User32.WindowMessage.WM_LBUTTONDOWN, new IntPtr(0x0001), lParam);
+            User32.PostMessage(windowHandle, User32.WindowMessage.WM_LBUTTONUP, new IntPtr(0x0000), lParam);
         }
 
         public User32.WINDOWINFO GetWindowInfo(IntPtr windowHandle)
